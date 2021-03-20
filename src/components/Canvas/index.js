a6601fdf72eb5d961e40b2cdfdc8aade65bb62a4
captainlonate <captainlonate@gmail.com>
2021-03-20 18:15:33 -0400

    put planks behind canvas too

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -9,10 +9,10 @@ const GameContainer = styled.div`
   right: 0;
   top: 0;
   bottom: 0;
-  /* background-image: url('images/backgrounds/simple_plants.png'); */
+  background-image: url('images/backgrounds/simple_plants.png');
   /* background-image: url('images/backgrounds/bird.png'); */
   /* background-image: url('images/backgrounds/fishbowl_4k.png'); */
-  background-image: url('images/backgrounds/kitchen_4k.png');
+  /* background-image: url('images/backgrounds/kitchen_4k.png'); */
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -44,19 +44,10 @@ const StyledCanvas = styled.canvas`
   box-sizing: border-box;
   width: 100%;
   height: 100%;
-  /* border: 30px solid blue;
-  border-image: url('images/canvas_frame.png');
-  border-image-slice: 33%;
-  border-image-width: 1;
-  border-image-outset: 0;
-  border-image-repeat: stretch; */
+  border: 5px solid #462a1a;
 `
 
 const CanvasAndFrameWrapper = styled.div`
-  position: relative;
-`
-
-const Planks = styled.div`
   position: relative;
 `
 
@@ -66,7 +57,7 @@ const TopPlank = styled.img`
   left: -5%;
   width: 110%;
   height: 8%;
-  z-index: 3;
+  z-index: 4;
   transform: rotate3d(1, 1, 1, 2deg);
 `
 
@@ -76,66 +67,73 @@ const BottomPlank = styled.img`
   left: -5%;
   width: 110%;
   height: 8%;
-  z-index: 3;
+  z-index: 4;
   transform: rotate3d(1, 1, 1, -1deg);
 `
 
 const LeftPlank = styled.img`
   position: absolute; 
-  top: -6%;
-  left: 2%;
+  bottom: -15%;
+  left: -8%;
   width: 160%;
   height: 10%;
   z-index: 3;
-  transform: rotate3d(0, 0, 1, 90deg);
+  transform: rotate3d(0, 0, 1, -92deg);
   transform-origin: top left;
 `
 
 const RightPlank = styled.img`
   position: absolute; 
-  top: -6%;
-  right: 1%;
+  bottom: -14%;
+  right: -12%;
   width: 160%;
   height: 10%;
   z-index: 3;
-  transform: rotate3d(0, 0, 1, -90deg);
+  transform: rotate3d(0, 0, 1, 91deg);
   transform-origin: top right;
 `
 
-// const CanvasFrame = styled.div`
-//   position: absolute;
-//   background-image: url('images/canvas_frame.png');
-//   background-repeat: no-repeat;
-//   background-size: contain;
-//   background-position: center;
+const TopPlankBehind = styled.img`
+  position: absolute;
+  top: -6%;
+  left: -9%;
+  width: 115%;
+  height: 12%;
+  z-index: -3;
+  transform: rotate3d(1, 1, 1, -3deg);
+`
 
-//   top: -20%;
-//   left: -20%;
-//   width: 100%;
-//   height: 100%;
+const BottomPlankBehind = styled.img`
+  position: absolute;
+  bottom: -7%;
+  left: -14%;
+  width: 125%;
+  height: 12%;
+  z-index: -3;
+  transform: rotate3d(0, 0, 1, 178deg);
+`
 
-//   z-index: -1;
-// `
+const LeftPlankBehind = styled.img`
+  position: absolute;
+  top: -6%;
+  left: 8%;
+  width: 160%;
+  height: 13%;
+  z-index: -3;
+  transform: rotate3d(0, 0, 1, 91deg);
+  transform-origin: top left;
+`
 
-// const CanvasFrame = styled.img`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   width: 0;
-//   height: 0;
-
-//   z-index: -1;
-// `
-
-// const CanvasFrame = styled.div`
-//   position: absolute;
-//   background-image: url('images/canvas_frame.png');
-//   background-repeat: no-repeat;;
-//   top: 0;
-//   left: 0;
-
-//   z-index: -1;
-// `
+const RightPlankBehind = styled.img`
+  position: absolute;
+  bottom: -17%;
+  right: -10%;
+  width: 160%;
+  height: 12%;
+  z-index: -3;
+  transform: rotate3d(0, 0, 1, 88deg);
+  transform-origin: top right;
+`
 
 const GameCanvas = () => {
   const canvasRef = useRef(null)
@@ -159,10 +157,16 @@ const GameCanvas = () => {
         <GameArea ref={gameAreaRef}>
           <CanvasAndFrameWrapper>
             <StyledCanvas id='gameCanvas' ref={canvasRef} width='500px' height='500px' />
+            {/* Foreground - In front of the canvas */}
             <TopPlank src='images/TopPlank.png' />
             <BottomPlank src='images/TopPlank.png' />
             <LeftPlank src='images/TopPlank.png' />
             <RightPlank src='images/TopPlank.png' />
+            {/* Background - Behind the canvas */}
+            <TopPlankBehind src='images/TopPlank.png' />
+            <BottomPlankBehind src='images/TopPlank.png' />
+            <LeftPlankBehind src='images/TopPlank.png' />
+            <RightPlankBehind src='images/TopPlank.png' />
           </CanvasAndFrameWrapper>
         </GameArea>
       </InnerWrapper>
